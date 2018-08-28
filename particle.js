1c76caf8e95a97901ca0c869a49989289ee8967d
kazimsanlav <kazimsanlav@gmail.com>
2018-08-28 17:59:10 +0300

    random -> noise

--- particle.js
+++ particle.js
@@ -1,22 +1,28 @@
 let MAXHIST = 100;
 
+
 function Particle(x, y) {
+
+    this.count = 0;
 
     this.coor = createVector(x, y);
 
     this.history = [];
 
     this.update = function () {
+        this.count += random()/15;
 
-        this.coor.x += random(-5, 10);
-        this.coor.y += random(-10, 10);
+        // this.coor.x += random(-5, 10);
+        this.coor.x += noise(this.count)*10-5;
+        this.coor.y += noise(this.count+100)*10-5;
+
         this.history.push(createVector(this.coor.x, this.coor.y));
 
-        for (var i = 0; i < this.history.length; i++) {
-            var pos = this.history[i];
-            pos.x += random(-2,2);
-            pos.y += random(-2,2);
-        }
+        // for (var i = 0; i < this.history.length; i++) {
+        //     var pos = this.history[i];
+        //     pos.x += random(-2,2);
+        //     pos.y += random(-2,2);
+        // }
         
         if(this.history.length > MAXHIST){
             this.history.splice(0, 1);
